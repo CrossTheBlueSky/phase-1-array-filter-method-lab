d5fdc1f0c4e7b5e05a1838ea8ca5699f2d4711a1
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-31 09:56:24 -0600

    filters arrays and objects

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    const foundMatches = drivers.filter((e)=>e.toUpperCase() === name.toUpperCase())
+    return foundMatches
+}
+
+function fuzzyMatch(drivers, name){
+    const foundMatches = drivers.filter((e)=> e.slice(0, name.length) === name)
+    return foundMatches
+}
+
+function matchName(drivers, name){
+    return drivers.filter((e)=> e.name === name)
+}
